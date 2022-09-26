6a83c2f437070eb8a7d2509b15617e674d4af193
Albert200024 <ghumashyan.albert@inbox.ru>
2022-09-26 15:15:34 +0400

    change header name

--- src/componetns/header/Header.jsx
+++ src/componetns/header/Header.jsx
@@ -9,12 +9,11 @@ const Header = () => {
     <header>
       <div className="container header_container">
             <h5>Hello I'm</h5>
-            <h1>Albert Ghumashyan</h1>
+            <h1>Hajia Bintu</h1>
             <h5 className="text-light">Frontend Developer</h5>
             <CTA/> 
             <HeaderSocials/>
             <div className="me">
-              
                 <img src={ME}alt=''/>
             </div>
             <a href='#contact' className='scroll_down'>Scroll Down</a>
